b756d5cd14e5e660733cb3a66861e35b1ef51852
muhamaddafa <gaming102dafa@gmail.com>
2023-10-20 11:33:02 +0700

    fix: change header value to development

--- php/header.php
+++ php/header.php
@@ -1,10 +1,9 @@
 <?php
 // PRODUCTION
-header("Access-Control-Allow-Origin: https://tasktracker.site");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Credentials: true");
+// header("Access-Control-Allow-Origin: https://tasktracker.site");
 
 // DEVELOPMENT
-// header("Access-Control-Allow-Origin: http://localhost:5173");
-// header("Access-Control-Allow-Headers: Content-Type");
-// header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Origin: http://localhost:5173");
+
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
